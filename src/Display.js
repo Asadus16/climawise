823e7e3fa53cea43811f35ae5533fb830e48b12e
Asadus16 <asadslayer999@gmail.com>
2024-09-09 00:09:39 +0500

    responsive

--- src/Display.js
+++ src/Display.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './Form';
 import Tabs from './components/tabs';
-import { fetchWeatherData } from './api'; // Import the API function
+import { fetchWeatherData } from './api';
 
 export default function Display() {
-  const [location, setLocation] = useState('Lahore');
+  const [location, setLocation] = useState('');
   const [weatherData, setWeatherData] = useState({
     current: { temp_c: 'Loading...' },
     location: { name: 'Lahore' },
@@ -14,21 +14,20 @@ export default function Display() {
   useEffect(() => {
     fetchWeatherData('Lahore')
       .then((data) => {
-        setWeatherData(data); // Set the fetched weather data for Lahore
+        setWeatherData(data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []); // Empty dependency array to ensure this runs once when the component mounts
+  }, []);
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (!location || location === '') return;
 
-    // Fetch weather data when the form is submitted
     fetchWeatherData(location)
       .then((data) => {
-        setWeatherData(data); // Update weather data when the user submits a location
+        setWeatherData(data);
       })
       .catch((error) => {
         console.log(error);
